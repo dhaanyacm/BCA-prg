06d6fe3da87a9cc6bb63261885fafbf61b6065d8
dhaanyacm <dhaanyacm03@gmail.com>
2026-04-08 21:28:52 +0530

    Update prime_numbers.py

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,10 +1,10 @@
 def primes(n):
-    for num in range(2,n+1):
-    for i in range(2,num):
-    if num % i == 0:
-        break
-    else:
-      yield num
+    for num in range(2, n+1):
+        for i in range(2, num):
+            if num % i == 0:
+               break
+        else:
+            yield num
 limit = int(input("generate prime numbers upto:" ))
 print(f"prime numbers upto", (limit))
 for prime in primes(limit):
